cbcf76915ab79732149fab6b452d277ee875197f
aalsu <aalsulami35@gmail.com>
2021-10-11 14:06:49 +0300

    Heads Up! Prep

---
+++ app/src/main/java/com/example/headsupprepapp/APIInterface.kt
@@ -0,0 +1,23 @@
+package com.example.headsupprepapp
+
+import com.example.headsupprepapp.moodle.Celebrity
+import retrofit2.Call
+import retrofit2.http.*
+
+interface APIInterface {
+    @GET("/celebrities/")
+    fun getCelebrities(): Call<ArrayList<Celebrity>>
+
+    @POST("/celebrities/")
+    fun addCelebrity(@Body celebrityData: Celebrity): Call<Celebrity>
+
+    @GET("/celebrities/{id}")
+    fun getCelebrity(@Path("id") id: Int): Call<Celebrity>
+
+    // PUT replaces the full object (use PATCH to change individual fields)
+    @PUT("/celebrities/{id}")
+    fun updateCelebrity(@Path("id") id: Int, @Body celebrityData: Celebrity): Call<Celebrity>
+
+    @DELETE("/celebrities/{id}")
+    fun deleteCelebrity(@Path("id") id: Int): Call<Void>
+}
---
+++ app/src/main/java/com/example/headsupprepapp/Add_New_celebrtActivity2.kt
@@ -0,0 +1,85 @@
+package com.example.headsupprepapp
+
+import android.app.ProgressDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.headsupprepapp.moodle.Celebrity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class Add_New_celebrtActivity2 : AppCompatActivity() {
+    private lateinit var etName: EditText
+    private lateinit var etTaboo1: EditText
+    private lateinit var etTaboo2: EditText
+    private lateinit var etTaboo3: EditText
+    private lateinit var btAdd: Button
+    private lateinit var btBack: Button
+    private val apiInterface by lazy { APIClient().getClient().create(APIInterface::class.java) }
+    private lateinit var progressDialog: ProgressDialog
+    private lateinit var existingCelebrities: ArrayList<String>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_celebrt2)
+        existingCelebrities = intent.extras!!.getStringArrayList("celebrityNames")!!
+
+        etName = findViewById(R.id.edtxtNewName)
+        etTaboo1 = findViewById(R.id.dtxttNewTabo1)
+        etTaboo2 = findViewById(R.id.edtxtNewTabo2)
+        etTaboo3 = findViewById(R.id.edtxtNewTabo3)
+
+        btAdd = findViewById(R.id.btNewAdd)
+        btAdd.setOnClickListener {
+            if(etName.text.isNotEmpty() && etTaboo1.text.isNotEmpty() &&
+                etTaboo2.text.isNotEmpty() && etTaboo3.text.isNotEmpty()){
+                addCelebrity()
+            }else{
+                Toast.makeText(this, "One or more fields is empty", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        btBack = findViewById(R.id.btnBack)
+        btBack.setOnClickListener {
+            intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun addCelebrity(){
+        progressDialog = ProgressDialog(this)
+        progressDialog.setMessage("Please Wait")
+        progressDialog.show()
+
+        apiInterface.addCelebrity(
+            Celebrity(
+                // This only takes care of the first name
+                // If Michael Jordan exists, Michael jordan can still be added
+                // Can you find a way to fix this?
+                etName.text.toString().capitalize(),
+                etTaboo1.text.toString(),
+                etTaboo2.text.toString(),
+                etTaboo3.text.toString(),
+                0
+            )
+        ).enqueue(object: Callback<Celebrity> {
+            override fun onResponse(call: Call<Celebrity>, response: Response<Celebrity>) {
+                progressDialog.dismiss()
+                if(!existingCelebrities.contains(etName.text.toString().lowercase())){
+                    intent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(this@Add_New_celebrtActivity2, "Celebrity Already Exists", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onFailure(call: Call<Celebrity>, t: Throwable) {
+                progressDialog.dismiss()
+                Toast.makeText(this@Add_New_celebrtActivity2, "Unable to get data", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+}
